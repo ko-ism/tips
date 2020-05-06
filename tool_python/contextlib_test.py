c5dd7a06e016b46f40189895f2b237f053dbccc5
shinya koizumi <shin.koizumi@nttcsol.com>
2020-05-06 09:54:17 +0900

    init

---
+++ tool_python/contextlib_test.py
@@ -0,0 +1,15 @@
+import contextlib
+
+def tag(f):
+    def _wrapper():
+        print('<h2>')
+        r = f()
+        print('</h2>')
+        return r
+    return _wrapper
+
+@tag
+def f():
+    print('test')
+
+f()

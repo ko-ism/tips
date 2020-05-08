1b10fd7c47a83cdc84541f4dea771ec47b2c680a
shinya koizumi <shin.koizumi@nttcsol.com>
2020-05-08 09:18:18 +0900

    init

--- tool_python/contextexit_test.py
+++ tool_python/contextexit_test.py
@@ -12,8 +12,16 @@ def is_ok_job():
 def cleanup():
     print('clean up')
 
+def cleanup2():
+    print('clean up2')
+
 with contextlib.ExitStack() as stack:
     stack.callback(cleanup)
+    stack.callback(cleanup2)
+
+    @stack.callback
+    def cleanup3():
+        print('clean up3')
 
     is_ok = is_ok_job()
     print('more task')

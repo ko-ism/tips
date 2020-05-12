63d5b899ec4a177a5e51de9c1dff3839ab928cd5
shinya koizumi <shin.koizumi@nttcsol.com>
2020-05-12 23:41:26 +0900

    init

---
+++ tool_python/requests_test.py
@@ -0,0 +1,15 @@
+import requests
+
+payload = {'key1': 'value1', 'key2': 'value2'}
+
+# r = requests.get('http://httpbin.org/get', params=payload)
+# r = requests.post('http://httpbin.org/post', params=payload)
+# r = requests.put('http://httpbin.org/put', params=payload)
+# r = requests.delete('http://httpbin.org/delete', params=payload)
+
+r = requests.get('http://httpbin.org/get', params=payload, timeout=0.01)
+
+print(r.status_code)
+print(r.text)
+
+print(r.json())
--- tool_python/rest_test.py
+++ tool_python/rest_test.py
@@ -14,8 +14,38 @@ import json
 
 payload = {'key1': 'value1', 'key2': 'value2'}
 
-url = 'http://httpbin.org/get' + '?' + urllib.parse.urlencode(payload)
-with urllib.request.urlopen(url) as f:
-    r = json.loads(f.read().decode('utf-8'))
+# # GET
+# url = 'http://httpbin.org/get' + '?' + urllib.parse.urlencode(payload)
+# with urllib.request.urlopen(url) as f:
+#     r = json.loads(f.read().decode('utf-8'))
+# print(r)
 
-print(r)
+
+# # POST
+# payload = json.dumps(payload).encode('utf-8')
+# print(payload)
+# req = urllib.request.Request(
+#     'http://httpbin.org/post', data=payload, method='POST'
+# )
+# with urllib.request.urlopen(req) as f:
+#     print(json.loads(f.read().decode('utf-8')))
+
+
+# # PUT
+# payload = json.dumps(payload).encode('utf-8')
+# print(payload)
+# req = urllib.request.Request(
+#     'http://httpbin.org/put', data=payload, method='PUT'
+# )
+# with urllib.request.urlopen(req) as f:
+#     print(json.loads(f.read().decode('utf-8')))
+
+
+# DELETE
+payload = json.dumps(payload).encode('utf-8')
+print(payload)
+req = urllib.request.Request(
+    'http://httpbin.org/delete', data=payload, method='DELETE'
+)
+with urllib.request.urlopen(req) as f:
+    print(json.loads(f.read().decode('utf-8')))

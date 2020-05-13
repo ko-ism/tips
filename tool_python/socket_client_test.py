682a01a001077f9433cbe23d48e9b39f2251d1e0
shinya koizumi <shin.koizumi@nttcsol.com>
2020-05-13 09:17:33 +0900

    init

--- tool_python/socket_client_test.py
+++ tool_python/socket_client_test.py
@@ -1,9 +1,14 @@
 import socket
 
-# TCP SOCKET
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('127.0.0.1', 50007))
-    s.sendall(b'Hello')
-    data = s.recv(1024)
-    print(repr(data))
-    print(data)
+# # TCP SOCKET
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect(('127.0.0.1', 50007))
+#     s.sendall(b'Hello')
+#     data = s.recv(1024)
+#     print(repr(data))
+#     print(data)
+
+
+# UDP SOCKET
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(b'Hello UDP', ('127.0.0.1', 50007))
--- tool_python/socket_server_test.py
+++ tool_python/socket_server_test.py
@@ -6,16 +6,26 @@
 
 import socket
 
-# TCP SOCKET
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# # TCP SOCKET
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind(('127.0.0.1', 50007))
+#     s.listen(1)
+#     while True:
+#         conn, addr = s.accept()
+#         with conn:
+#             while True:
+#                 data = conn.recv(1024)
+#                 if not data:
+#                     break
+#                 print('data:{}, addr:{}'.format(data, addr))
+#                 conn.sendall(b'Received: ' + data)
+
+
+# UDP SOCKET
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind(('127.0.0.1', 50007))
-    s.listen(1)
     while True:
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print('data:{}, addr:{}'.format(data, addr))
-                conn.sendall(b'Received: ' + data)
+        data, addr  = s.recvfrom(1024)
+        if not data:
+            break
+        print('data:{}, addr:{}'.format(data, addr))

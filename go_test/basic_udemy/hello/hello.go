18fcbb50b178640313af2d5bfacc1962fd9d7e11
shinya koizumi <shin.koizumi@nttcsol.com>
2020-06-01 08:51:45 +0900

    init

---
+++ go_test/basic_udemy/hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+func init() {
+	fmt.Println("init")
+}
+*/
+
+func bazz() {
+	fmt.Println("Bazz")
+}
+
+func main() {
+	bazz()
+	fmt.Println("Hello world Println", "TEST")
+}

b0cb68b5a629c4a903f924bc5d028b652803e4f9
shinya koizumi <shin.koizumi@nttcsol.com>
2020-06-10 11:30:27 +0900

    init

---
+++ go_test/basic_udemy/package/bitcoin_zaif/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func main() {
+    uri := "https://api.zaif.jp/api/1/last_price/btc_jpy"
+    req, _ := http.NewRequest("GET", uri, nil)
+
+    client := new(http.Client)
+    resp, _ := client.Do(req)
+    defer resp.Body.Close()
+
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println(string(byteArray))
+}
